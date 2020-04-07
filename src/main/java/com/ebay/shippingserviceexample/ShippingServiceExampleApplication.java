05c1daba5ad2333fc756b5b7beab63084b2ca5bd
Lukas Dahlberg <Sarlacc83@gmail.com>
2020-04-07 14:19:55 -0700

    shipping-service-example Initial commit

---
+++ src/main/java/com/ebay/shippingserviceexample/ShippingServiceExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ebay.shippingserviceexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingServiceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingServiceExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebay/shippingserviceexample/ShippingServiceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebay.shippingserviceexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingServiceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
